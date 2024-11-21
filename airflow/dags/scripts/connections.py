b18615a8d950d714acb9833d7ea0353282069a9f
Garett Kaube <gkaube@outlook.com>
2024-11-21 15:31:01 -0700

    update

--- airflow/dags/scripts/connections.py
+++ airflow/dags/scripts/connections.py
@@ -14,8 +14,6 @@ def get_secret(secret_name):
     -------
     - list of secret values
     """
-
-    # Create a Secrets Manager client
     client = boto3.client("secretsmanager")
     response = client.get_secret_value(SecretId=secret_name)
 
